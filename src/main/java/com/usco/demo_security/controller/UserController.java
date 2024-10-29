d58ea570febd7bba9533e0f2a9d49e57836e2b44
michelcardona <u20231211735@usco.edu.co>
2024-10-29 08:10:15 -0500

    final

--- src/main/java/com/usco/demo_security/controller/UserController.java
+++ src/main/java/com/usco/demo_security/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 @RequestMapping
 public class UserController {
-    
+
 
     @GetMapping("/login")
     public String login() {
